a30e9be18a01f06c769aea6f5665b97f50c58321
Robert Martin <rmartin@rmart.in>
2016-12-01 19:29:39 +0000

    Initial commit

---
+++ atlas-mapbook/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import Mapbook from 'mapbook'
+// import './index.css';
+
+ReactDOM.render(
+  <Mapbook />,
+  document.getElementById('root')
+);
---
+++ mapbook/src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+export default class Mapbook extends Component {
+  render() {
+    return (
+      <div>This is a Mapbook.</div>
+    )
+  }
+}
